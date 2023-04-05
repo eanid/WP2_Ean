fc2b5548f21762f67bb3b91a337e65e9a6d64969
eanp <awifamz@gmail.com>
2023-04-05 20:39:51 +0700

    add dynamic route

--- application/controllers/DataMhs.php
+++ application/controllers/DataMhs.php
@@ -5,9 +5,20 @@ class DataMhs extends CI_Controller{
     {
         $data= $this->db->query('select * from mahasiswa');
         foreach($data->result_array() as $d){
-        echo "Nim mahasiswa : ".$d['nim']."<br />";
+        echo "<a href='http://creads.ean/datamhs/view/".$d['nim']."'>Nim mahasiswa : ".$d['nim']."<br />";
         echo "Nama mahasiswa : ".$d['nama']."<br />";
-        echo "Kelas mahasiswa : ".$d['kelas']."<br />";
+        echo "Kelas mahasiswa : ".$d['kelas']."<br /></a><br />";
         }
+    }
+
+    public function view($slug = NULL)
+    {
+        $data = $this->db->get_where('mahasiswa', array('nim' => $slug));
+        foreach($data->result_array() as $d){
+            echo "<a href='http://creads.ean/datamhs'><button>back</button></a><br />";
+            echo "Nim mahasiswa : ".$d['nim']."<br />";
+            echo "Nama mahasiswa : ".$d['nama']."<br />";
+            echo "Kelas mahasiswa : ".$d['kelas']."<br />";
+            }
     }
 }
