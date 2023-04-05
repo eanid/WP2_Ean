d00b0881092674bc1dbee706fd12de828f064d95
eanp <awifamz@gmail.com>
2023-04-05 20:04:16 +0700

    fix autoload

--- application/controllers/DataMhs.php
+++ application/controllers/DataMhs.php
@@ -1,12 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DataMhs extends CI_Controller{
-    public function __construct()
-    {
-        parent::__construct(); 
-        $this->load->database();
-    }
-    
+class DataMhs extends CI_Controller{    
     public function index()
     {
         $data= $this->db->query('select * from mahasiswa');
@@ -16,13 +10,4 @@ class DataMhs extends CI_Controller{
         echo "Kelas mahasiswa : ".$d['kelas']."<br />";
         }
     }
-
-    // public function index(){
-    //     // $data=$this->db->query('select * from mahasiswa');foreach($data->result_array() as $d){
-    //     //     echo "Nim mahasiswa : ".$d['nim']."<br />";
-    //     //     echo "Nama mahasiswa : ".$d['nama']."<br />";
-    //     //     echo "Kelas mahasiswa : ".$d['kelas']."<br />";
-    //     // }
-    //     echo"wow";
-    // }
 }
