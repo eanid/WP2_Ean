f0bdf7570aaa5b454de6359b5f3ec947a4a998c7
eanp <awifamz@gmail.com>
2023-05-03 17:32:33 +0700

    env example

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -9,7 +9,7 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<title><?php echo lang('login_heading'); ?></title>
+	<title>Login Creads</title>
 
 	<!-- Custom fonts for this template-->
 	<link href="<?php echo base_url("sbadmin/") ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
